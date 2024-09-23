2e3358f2b5091476578ff8173f7af111052ae8c2
Amir Y. Perehodnik <myrprhwdnyq@gmail.com>
2024-09-23 04:49:28 +0300

    work already jesus

--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-java -jar ./mcp.jar setup a1.2.6
-java -jar ./mcp.jar decompile
-java -jar ./mcp.jar applypatch
-java -jar ./mcp.jar recompile
+java -jar $(pwd)/mcp.jar setup a1.2.6
+java -jar $(pwd)/mcp.jar decompile
+java -jar $(pwd)/mcp.jar applypatch
+java -jar $(pwd)/mcp.jar recompile
 
 echo "Manifest-Version: 1.0" > MANIFEST.MF
 echo "Main-Class: net.minecraft.server.MinecraftServer" >> MANIFEST.MF
