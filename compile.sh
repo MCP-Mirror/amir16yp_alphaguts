85682991a898db5559a2df4c33f8252e85369b4e
Amir Y. Perehodnik <myrprhwdnyq@gmail.com>
2024-09-23 04:32:36 +0300

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-java -jar mcp.jar setup a1.2.6
-java -jar mcp.jar decompile
-java -jar mcp.jar applypatch
-java -jar mcp.jar recompile
+
+echo $(which java)
+
+/usr/bin/java -jar mcp.jar setup a1.2.6
+/usr/bin/java -jar mcp.jar decompile
+/usr/bin/java -jar mcp.jar applypatch
+/usr/bin/java -jar mcp.jar recompile
 
 echo "Manifest-Version: 1.0" > MANIFEST.MF
 echo "Main-Class: net.minecraft.server.MinecraftServer" >> MANIFEST.MF
