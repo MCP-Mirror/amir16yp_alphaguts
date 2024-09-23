1fc8bec1922643153d95f02bfc3889031f6cbaa1
Amir Y. Perehodnik <myrprhwdnyq@gmail.com>
2024-09-23 04:23:10 +0300

    Add compilation scripts

---
+++ compile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+java -jar mcp.jar setup a1.2.6
+java -jar mcp.jar decompile
+java -jar mcp.jar applypatch
+java -jar mcp.jar recompile
+
+echo "Manifest-Version: 1.0" > MANIFEST.MF
+echo "Main-Class: net.minecraft.server.MinecraftServer" >> MANIFEST.MF
+
+jar cfm server.jar MANIFEST.MF -C minecraft_server/bin/ .
