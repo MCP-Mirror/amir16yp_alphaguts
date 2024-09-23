be8d0bbd8e4949f36d7069b77f763a7c466ca7fc
Amir Y. Perehodnik <myrprhwdnyq@gmail.com>
2024-09-23 04:46:54 +0300

    please work for the love of god

--- compile.sh
+++ compile.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-echo $(which java)
-
-ls -la
-
-/usr/bin/java -jar ./mcp.jar setup a1.2.6
-sleep 1
-/usr/bin/java -jar ./mcp.jar decompile
-sleep 1
-/usr/bin/java -jar ./mcp.jar applypatch
-sleep 1
-/usr/bin/java -jar ./mcp.jar recompile
-sleep 1
+java -jar ./mcp.jar setup a1.2.6
+java -jar ./mcp.jar decompile
+java -jar ./mcp.jar applypatch
+java -jar ./mcp.jar recompile
 
 echo "Manifest-Version: 1.0" > MANIFEST.MF
 echo "Main-Class: net.minecraft.server.MinecraftServer" >> MANIFEST.MF
